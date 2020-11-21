37ba83025abaad57cefa8c9631ffec8cc43155fc
David Taylor <45290148+deeteeakl@users.noreply.github.com>
2020-11-21 15:35:11 +0000

    Update motor.py

--- motor.py
+++ motor.py
@@ -3,29 +3,32 @@ from kivy.uix.gridlayout import GridLayout
 from speedmeter import SpeedMeter
 from kivy.uix.floatlayout import FloatLayout
 from kivy.clock import Clock
-import RPi.GPIO as GPIO
+import sys
 
-# define the pins connected to L293D 
+if sys.platform.startswith('linux'):
+    import RPi.GPIO as GPIO
+
+# define the pins connected to L293D
 _MOTORPIN1 = 13
 _MOTORPIN2 = 11
 _ENABLEPIN = 15
 _MINSPEED = 0
 _MAXSPEED = 100
-motorStatus = False
 
-    
+
 class Motor():
     def __init__(self, speed=0):
-        GPIO.cleanup()
-        GPIO.setmode(GPIO.BOARD)   
-        GPIO.setup(_MOTORPIN1,GPIO.OUT)   # set pins to OUTPUT mode
-        GPIO.setup(_MOTORPIN2,GPIO.OUT)
-        GPIO.setup(_ENABLEPIN,GPIO.OUT)
-        self.__p = GPIO.PWM(_ENABLEPIN,1000) # creat PWM and set Frequency to 1KHz        self.__on = False
+        if sys.platform.startswith('linux'):
+            GPIO.cleanup()
+            GPIO.setmode(GPIO.BOARD)
+            GPIO.setup(_MOTORPIN1, GPIO.OUT)   # set pins to OUTPUT mode
+            GPIO.setup(_MOTORPIN2, GPIO.OUT)
+            GPIO.setup(_ENABLEPIN, GPIO.OUT)
+            # creat PWM and set Frequency to 1KHz        self.__on = False
+            self.__p = GPIO.PWM(_ENABLEPIN, 1000)
         self.speed = speed
-        self.__p.start(self.speed)
         self.__on = False
-        print("motor instantiated")
+        #print("motor instantiated")
 
     def __repr__(self):
         return "Motor %s, speed %s" % (self.power, self.speed)
@@ -51,40 +54,35 @@ class Motor():
     def speed(self, x):
         if x > _MAXSPEED:
             x = _MAXSPEED
-        print(x)
         self.__speed = x
+        if sys.platform.startswith('linux'):
+            if self.__on:
+                # motoRPin1 output HIHG level
+                GPIO.output(_MOTORPIN1, GPIO.HIGH)
+                # motoRPin2 output LOW level
+                GPIO.output(_MOTORPIN2, GPIO.LOW)
+                if self.speed > 0:
+                    print('Turn Forward...%s' % (self.speed))
+                    self.__p.start(self.speed)
+            else:
+                GPIO.output(_MOTORPIN1, GPIO.LOW)
+                GPIO.output(_MOTORPIN2, GPIO.LOW)
+                print('Motor Stop...')
 
-    def turn(self):
-        if self.__on:
-            GPIO.output(_MOTORPIN1,GPIO.HIGH)  # motoRPin1 output HIHG level
-            GPIO.output(_MOTORPIN2,GPIO.LOW)   # motoRPin2 output LOW level
-            if self.speed > 0:
-                print ('Turn Forward...%s' % (self.speed))
-                self.__p.start(self.speed)
-        else:
-            GPIO.output(_MOTORPIN1,GPIO.LOW)
-            GPIO.output(_MOTORPIN2,GPIO.LOW)
-            print ('Motor Stop...')
 
-
-class MotorController(FloatLayout, Motor):
+class MotorController(FloatLayout):
     def __init__(self, **kwargs):
         super(MotorController, self).__init__(**kwargs)
         battery = self.ids.battery
         battery.value = 99
         battery.value = 100
         self.clockRunning = True
-        self.power =True
-        #self.speed = 10
-        #print(self.speed)
-        
         Clock.schedule_interval(self.tick, 0.5)
-        print("motor controller instantiated")
 
     def tick(self, *args, **kwargs):
         battery = self.ids.battery
-        battery.progress_color = (0, 1, 0, 1)
         speed_value = self.ids.speed_value
+        power_button = self.ids.power_button
 
         if speed_value.value > 0:
             self.__on = True
@@ -94,26 +92,41 @@ class MotorController(FloatLayout, Motor):
             else:
                 battery.value = new_battery_value
 
-            if battery.value <= battery.min and speed_value.value > speed_value.min:
-                speed_value.value = speed_value.value * 0.9 - 3
+            if (battery.value <= battery.min or power_button.active == False) and speed_value.value > speed_value.min:
+                speed_value.value = speed_value.value * 0.9 - 5
                 if speed_value.value <= speed_value.min:
                     speed_value.value = speed_value.min
                     speed_value.disabled = True
-        self.turn()
+
+        if power_button.active == False and battery.value < 100:
+            new_battery_value = battery.value + 1
+            if new_battery_value < battery.max:
+                battery.value = new_battery_value
+            else:
+                battery.value = battery.max
 
 
 class motor_controller(App):
+    def __init__(self, motor, **kwargs):
+        super(motor_controller, self).__init__(**kwargs)
+        self.motor = motor
+
     def build(self):
         return MotorController()
 
     def set_speed(self):
         speed_value = self.root.ids.speed_value
-        #print(speed_value.value)
-        MotorController.speed = speed_value.value
+        self.motor.speed = speed_value.value
+
+    def power_button(self, instance, value):
+        speed_value = self.root.ids.speed_value
+        self.motor.power = value
+        speed_value.disabled = not value
+
+        # print(self.motor.power)
 
 
 # run the app
 if __name__ == '__main__':
-    #motor = Motor()
-    motor_controller().run()
-
+    motor = Motor()
+    motor_controller(motor).run()
